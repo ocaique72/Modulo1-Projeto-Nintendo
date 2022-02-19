7d54fe46cd1966afd7db6858b5cd822c87de09d7
Caique Araujo <kiq901@hotmail.com>
2022-02-19 18:46:59 -0300

    att de organizacao

---
+++ scriptIndex.js
@@ -0,0 +1,12 @@
+// -----FUNÇÃO PARA ENTRAR NAS FASES DE CADA PERSONAGENS-----
+
+function fase1() {
+  location.assign("./marioFases/fase1.html");
+}
+function fase1Luigi() {
+  location.assign("./luigiFases/luigi-fase1.html");
+}
+
+function fase1Peach() {
+  location.assign("./peachFases/peach-fase1.html");
+}
